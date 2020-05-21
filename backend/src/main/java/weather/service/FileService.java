9cb0a1abb9c5361eb0a3aea938d465626663d336
Gerold Busch <gerold.busch@gmail.com>
2020-05-21 18:50:05 +0200

    add missing decorator

--- backend/src/main/java/weather/service/FileService.java
+++ backend/src/main/java/weather/service/FileService.java
@@ -1,10 +1,12 @@
 package weather.service;
 
+import org.springframework.stereotype.Service;
 import weather.model.Price;
 import weather.repository.FileRepository;
 
 import java.io.IOException;
 
+@Service
 public class FileService {
     public Iterable<Price> getPrices() throws IOException {
         return FileRepository.getPrices();
