f36ab661724249186cbdbbbdc50449117db4734e
Gerold Busch <gerold.busch@gmail.com>
2020-05-21 17:06:46 +0200

    trying to get CI run

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ set -e
 
 mkdir -p test-tmp/backend/config
 
-COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose-test.yml up --build "$@"
+COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose-test.yml up "$@"
